febcd0b30e40b16c931abce3ee04a8b5d658b001
jaskiewm <m.jasski@hotmail.com>
2024-04-23 20:35:19 -0400

    Updated OGL to Display 3 Triangles & Comments
    - Updated OGL to Display 3 Triangles (Zelda Triforce!)
    - Added comments throughout the main CPP file
    - Additional objects, text files, and logs to ensure separate CPP and Header files work with the main CPP

--- OpenGL/Main.cpp
+++ OpenGL/Main.cpp
@@ -7,37 +7,40 @@
 #include"VBO.h"
 #include"EBO.h"
 
+//-------Initializing vertices and indices-------//
+
+//Vertices of three triangles in a bigger triangle (Zelda)
+GLfloat vertices[]{
+	//Coordinates are Y,X,Z
+	//Every three points is one coordinate, and z = 0f because we are making 2D objects
+	//base = 0.5, height = 0.5
+
+	-0.5f, -0.5f, 0.0f,	// Lower Left Corner (COORD - 0)
+	0.5f, -0.5f, 0.0f,	// Lower Right Corner (COORD - 1)
+	0.0f, 0.5f, 0.0f,	// Top Corner (COORD - 2)
+	-0.25f, 0.0f, 0.0f, // Center Left Corner (COORD - 3)
+	0.25f, 0.0f, 0.0f,	// Center Right Corner (COORD - 4)
+	0.0f, -0.5f, 0.0f	// Bottom Corner (COORD - 5)
+};
+
+// Indices for 3 triangles in a larger triangle
+GLuint indices[] =
+{
+	0, 3, 5, //Bottom Left Triangle
+	3, 2, 4, //Bottom Right Triangle
+	5, 4, 1  //Top Triangle
+};
+
 int main() 
 {
 	//Initialize GLFW
 	glfwInit();
 
-	//Tells GLFW what version of OpenGL we are running
+	// Tells GLFW what version of OpenGL we are running
+	// OpenGL version is 3.3
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3); 
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE); //Package of functions from OpenGL (Core)
-
-	//Vertices of three triangles in a bigger triangle (Zelda)
-	GLfloat vertices[]{
-		//Lower left corner, lower right corner, top corner
-		//Every three points is one coordinate, and z = 0f because we are making 2D objects
-		-0.5f,-0.5f * float(sqrt(3)) / 3, 0.0f, // Lower Left Corner (0)
-		0.5f, -0.5f * float(sqrt(3)) / 3, 0.0f, // Lower Right Corner (1)
-		0.0f, 0.5f * float(sqrt(3)) * 2 / 3,0.0f // Top Corner (2)
-
-		- 0.5f / 2, 0.5f * float(sqrt(3)) / 6, 0.0f, // Center Left Corner (3)
-		0.5f / 2, 0.5f * float(sqrt(3)) / 6, 0.0f, // Center Right Corner (4)
-		0.0f, -0.5f * float(sqrt(3)) * 3 / 3,0.0f // Bottom Corner (5)
-	};
-
-	// Indices for 3 triangles in a larger triangle
-	GLuint indices[] =
-	{
-		0, 3, 5, //Bottom Left Triangle
-		3, 2, 4, //Bottom Right Triangle
-		5, 4, 1  //Top Triangle
-	};
-
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE); //Tell GLFW we are using CORE profile
 
 	//-------Loading window and viewport-------//
 	//Creating a window (width, height, name, full screen, not important)
@@ -79,18 +82,13 @@ int main()
 	VBO1.Unbind();
 	EBO1.Unbind();
 
-	//-------Window Settings-------//
-	//Specify background colour
-	glClearColor(0.07, 0.13f, 0.17f, 1.0f);
-	//Clean and assign to colour to BACK BUFFER
-	glClear(GL_COLOR_BUFFER_BIT);
-	//swap back buffer and front buffer
-	glfwSwapBuffers(window);
 
 	//Only close window when close button or another function is reached
 	while (!glfwWindowShouldClose(window)) {
 
+		// Specifies background colour
 		glClearColor(0.07, 0.13f, 0.17f, 1.0f);
+		// Clean the back buffer and assign colour to it
 		glClear(GL_COLOR_BUFFER_BIT);
 
 		//Tell OGL which shader program we want to use
@@ -98,9 +96,9 @@ int main()
 		// Bind the VAO so OGL knows how to use it
 		VAO1.Bind();
 		// Draw triangle using the GL_TRIANGLES primitive
-		glDrawArrays(GL_TRIANGLES, 0, 3); //Specifies primitive, starting index, number of vertices
-		glfwSwapBuffers(window); //Ensure buffers are swapped so we get a new image each frame
+		glDrawElements(GL_TRIANGLES, 9, GL_UNSIGNED_INT, 0); //Specifies primitive, number of indices, type of indices, starting index
 
+		glfwSwapBuffers(window); //Ensure buffers are swapped so we get a new image each frame
 		glfwPollEvents(); //Process all GLFW events
 	}
 
